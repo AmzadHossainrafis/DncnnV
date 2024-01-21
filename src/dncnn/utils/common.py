02df51095438f1b511ca51092b4978853f5bb11f
Amzad hossain <amzad.rafi@northsouth.edu>
2024-01-21 18:12:59 +0600

    Refactor common.py module

--- src/dncnn/utils/common.py
+++ src/dncnn/utils/common.py
@@ -1,48 +1,43 @@
-import yaml 
-import os 
+import yaml
+import os
 import numpy as np
 
+
 def read_config(config_path):
-
-    '''
-    arg : 
+    """
+    arg :
     config_path : path to the config file
-    
-    return : 
-    config : config file in the form of dictionary    
-    
-    
-    '''
-    with open(config_path) as f:
-        config = yaml.safe_load(f)
-    return config 
 
+    return :
+    config : config file in the form of dictionary
 
-def create_dir(path): 
 
     """
-    arg : 
-    path : path to the directory 
+    with open(config_path) as f:
+        config = yaml.safe_load(f)
+    return config
 
-    return : 
+
+def create_dir(path):
+    """
+    arg :
     path : path to the directory
-    
+
+    return :
+    path : path to the directory
+
     """
     if not os.path.exists(path):
         os.makedirs(path)
-    return path 
-
+    return path
 
 
 def lr_sheduler(optimizer, epoch, lr, decay_rate, decay_epoch):
     """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
     lr = lr * (decay_rate ** (epoch // decay_epoch))
     for param_group in optimizer.param_groups:
-        param_group['lr'] = lr
+        param_group["lr"] = lr
     return optimizer
-
-
-
 
 
 def denormalize(img, mean, std):
@@ -67,6 +62,7 @@ def denormalize(img, mean, std):
     std = np.array(std).reshape(3, 1, 1)
     denorm_img = std * img + mean
     return denorm_img
+
 
 # Usage:
 # denormalized_img = denormalize(normalized_img, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
